0f5c9b1157fd4f24293d3087c662cad5b6eb8305
Desmond Wong <d3smondwong@gmail.com>
2024-04-22 11:24:32 +0800

    Update: spelling error on Error initiation

--- src/exception.py
+++ src/exception.py
@@ -34,6 +34,6 @@ if __name__ == "__main__":
     try:
         a=1/0
     except Exception as e:
-        logging.info("Error iniation: Divide by zero")    
+        logging.info("Error initiation: Divide by zero")    
         raise CustomException(e,sys)               
 """
